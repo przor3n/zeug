0914d5b7588f1bfb35a67c0eed08382ecf789287
redcat7 <przemyslaw.kot@gmail.com>
2018-11-27 22:06:59 +0100

    fixed definition

--- zeug/zeug.py
+++ zeug/zeug.py
@@ -3,28 +3,33 @@ import logging
 from zeug.zeug_tools import Tool
 
 class Zeug(object):
-    def __init__(self, slots, instructions=None):
-        self.slots = slots
+    def __init__(self, tools, slots, instructions):
+        self.tools = tools
         self.environment = {}
+        self.environment.update(tools)
         self.environment.update(slots)
         self.instructions = instructions
         self.output = None
         self.on = True
         self.single_run = True
-
+    
     def prepare(self, *args):
         raise NotImplementedError("You have to implement me")
 
+    def on_exception(self, exception):
+        raise NotImplementedError("You have to implement me")
+    
+    
     def do(self):
         while self.on:
             try:
                 exec(self.instructions, globals(), self.environment)
             except Exception as e:
                 logging.error(e)
+                self.on_exception(e)
                 raise e
             finally:
                 self.update_state()
-                self.update_instructions()
 
     def update_state(self):
         if self.single_run:
@@ -32,9 +37,6 @@ class Zeug(object):
 
         self.output = self.environment.get('output', None)
 
-    def update_instructions(self):
-        pass
-
     def clean(self):
-        for name, tool in filter(lambda n,t: isinstance(t, Tool), self.slots.items()):
-            tool.clean()
+        for key, tool in self.tools.items()
+            tool.clean()   
